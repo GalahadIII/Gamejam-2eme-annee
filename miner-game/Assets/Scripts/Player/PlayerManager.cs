c163a15e488112b1c71946d7e55da5ad52ad8d06
Rockou_ <82128803+Rockouunderscore@users.noreply.github.com>
2023-11-24 22:16:58 -0500

    script 2

--- miner-game/Assets/Scripts/Player/PlayerManager.cs
+++ miner-game/Assets/Scripts/Player/PlayerManager.cs
@@ -1,12 +1,18 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D), typeof(MovementController2D))]
-public class PlayerManager : MonoBehaviour
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D), typeof(InputManager))]
+public class PlayerManager2D : MonoBehaviour, IPlayerManager2D
 {
+    #region IPlayerManager2D
+    public Vector2 Move_Live => frameInput.Movement2d.Live;
+    public bool Action1_FixedOnDown => frameInput.Action1.FixedOnDown;
+    public bool Rigidbody2D => rb;
+    #endregion
 
-#region SETTINGS
+    #region SETTINGS
 
+    [Header("Settings : Movement 2d (left,right,up,down)")]
     [SerializeField]
     private float moveSpeed = 20f;
 
@@ -19,78 +25,39 @@ public class PlayerManager : MonoBehaviour
     [SerializeField]
     private float velPower = 1.2f;
 
-#endregion
-
-#region SERIAL
-
-#endregion
-
-#region INST
-
-    private Rigidbody2D _rb;
-    private InputManager _input;
-    private PlayerInputs _frameInput;
-
-#endregion
-
-    #region Private
-
-    [SerializeField] private LayerMask _groundLayerMask;
-
-
-    private int _fixedUpdateCounter;
-    private bool _hasControl;
-    private bool _grounded;
-    private bool _facingPositiveX;
-    private float _gravityScale;
-    private BoxCollider2D _feetCollider;
-    private CapsuleCollider2D _headCollider;
+    [Header("Serialize")]
+    [SerializeField]
+    private Transform visual;
 
     #endregion
 
-    #region Public
+    #region INST
 
-    public Vector2 Input => _frameInput.Movement2d.Live;
-    public Vector2 Speed => _rb.velocity;
-    public bool Falling => _rb.velocity.y < 0;
-    public bool Jumping => _rb.velocity.y > 0;
-    public bool FacingPositiveX => _facingPositiveX;
-    public bool Grounded => _grounded;
-    public bool Rolling => _rolling;
-    public bool Dashing => _dashing;
+    private Rigidbody2D rb;
+    private InputManager input;
+    private PlayerInputs frameInput;
 
     #endregion
+
+    private bool hasControl;
+    private bool facingPositiveX;
 
     private void OnEnable()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _input = GetComponent<InputManager>();
-        _feetCollider = GetComponent<BoxCollider2D>();
-    }
-
-    private void Update()
-    {
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<InputManager>();
     }
 
     private void FixedUpdate()
     {
-        _fixedUpdateCounter++;
-
-        // input dependant
-        Flip();
-        Horizontal();
-        // Dodge();
-        ArtificialFriction();
+        FlipX();
+        Movement2d();
     }
 
-    #region Horizontal
-
-    private void Horizontal()
+    private void Movement2d()
     {
-        if (_dashing || _rolling) return;
-
-        Vector2 input = _frameInput.Movement2d.Live;
-        Vector2 vel = _rb.velocity;
+        Vector2 input = frameInput.Movement2d.Live;
+        Vector2 vel = rb.velocity;
 
         // calculate wanted direction and desired velocity
         Vector2 targetSpeed = input * moveSpeed;
@@ -101,40 +68,39 @@ public class PlayerManager : MonoBehaviour
 
         // applies acceleration to speed difference, raise to a set power so acceleration increase with higher speed
         // multiply by sign to reapply direction
-        float movementX = Mathf.Pow(Mathf.Abs(speedDif.x) * accelRate, velPower) * Mathf.Sign(speedDif.x);
-        float movementY = Mathf.Pow(Mathf.Abs(speedDif.y) * accelRate, velPower) * Mathf.Sign(speedDif.y);
-        Vector2 movement = new (movementX, movementY);
+        float movementX =
+            Mathf.Pow(Mathf.Abs(speedDif.x) * accelRate, velPower) * Mathf.Sign(speedDif.x);
+        float movementY =
+            Mathf.Pow(Mathf.Abs(speedDif.y) * accelRate, velPower) * Mathf.Sign(speedDif.y);
+        Vector2 movement = new(movementX, movementY);
 
         // apply the movement force
-        _rb.AddForce(movement, ForceMode2D.Force);
-        Debug.Log($"{input} / {movement} / {_rb.velocity}");
+        rb.AddForce(movement, ForceMode2D.Force);
+        // Debug.Log($"{input} / {movement} / {_rb.velocity}");
     }
-
-    #endregion
 
     private void FlipX()
     {
-        if (Mathf.Abs(Input.x) < 0.1f) return;
-        if (_facingPositiveX && Mathf.Sign(Input.x) < 0) return;
-        if (!_facingPositiveX && Mathf.Sign(Input.x) > 0) return;
+        Vector2 input = frameInput.Movement2d.Live;
+        if (Mathf.Abs(input.x) < 0.1f)
+            return;
+        if (facingPositiveX && Mathf.Sign(input.x) < 0)
+            return;
+        if (!facingPositiveX && Mathf.Sign(input.x) > 0)
+            return;
 
         Vector3 newScale = transform.localScale;
-        if (Mathf.Sign(Input.x) == Mathf.Sign(newScale.x))
-
-        newScale.x *= _facingPositiveX ? 1 : -1;
+        if (Mathf.Sign(input.x) == Mathf.Sign(newScale.x))
+            newScale.x *= facingPositiveX ? 1 : -1;
         transform.localScale = newScale;
 
-        _facingPositiveX = !_facingPositiveX;
+        facingPositiveX = !facingPositiveX;
     }
 }
 
-public interface IPlayerController
+public interface IPlayerManager2D
 {
-    public Vector2 Input { get; }
-    public Vector2 Speed { get; }
-    public bool Falling { get; }
-    public bool Jumping { get; }
-    public bool Rolling { get; }
-    public bool Dashing { get; }
-    public bool Grounded { get; }
+    public Vector2 Move_Live { get; }
+    public bool Action1_FixedOnDown { get; }
+    public bool Rigidbody2D { get; }
 }
