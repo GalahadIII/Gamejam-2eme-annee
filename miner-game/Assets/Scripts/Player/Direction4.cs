bcf3ec05baa6f901ad2b0ad8f093c1702725ea13
Rockou_ <82128803+Rockouunderscore@users.noreply.github.com>
2023-11-25 14:12:55 -0500

    dir4

--- miner-game/Assets/Scripts/Player/Direction4.cs
+++ miner-game/Assets/Scripts/Player/Direction4.cs
@@ -1,15 +1,15 @@
 
 public enum FacingDirection4
 {
-    Up = 0,
     North = 0,
+    Up = 0,
 
-    Right = 1,
     East = 1,
+    Right = 1,
 
-    Down = 2,
     South = 2,
+    Down = 2,
 
-    Left = 3,
     West = 3,
+    Left = 3,
 }
