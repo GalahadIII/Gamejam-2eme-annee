ad8cbc5e34857dd34313f264b3461143430bb236
Rockou_ <82128803+Rockouunderscore@users.noreply.github.com>
2023-11-25 22:56:52 -0500

    goofin' aroun'

--- miner-game/Assets/Scripts/MineralVein.cs
+++ miner-game/Assets/Scripts/MineralVein.cs
@@ -3,18 +3,55 @@ using UnityEngine;
 public class MineralVein : MonoBehaviour
 {
 
+    [SerializeField]
+    protected Transform visual;
+    [SerializeField]
+    protected float shakeTime = 0.3f;
+    [SerializeField]
+    protected float shakePower = 0.03f;
+
+    protected float lastMine = 0;
+    protected Vector3 originalPosition;
+
     #region mineral properties
 
+    [Header("Mineral settings")]
+    [SerializeField]
+    protected float integrity = 3f;
+    [SerializeField]
     protected float hardness = 1f;
 
     #endregion
 
-    private void OnEnable() { }
-    private void FixedUpdate() { }
+    private void OnEnable()
+    {
+        lastMine = -shakeTime;
+        originalPosition = visual.localPosition;
+    }
+    private void FixedUpdate()
+    {
+        if (lastMine + shakeTime > Time.time)
+        {
+            visual.localPosition = originalPosition + new Vector3(Random.Range(-shakePower, shakePower), Random.Range(-shakePower, shakePower), 0);
+        }
+    }
 
     public void Mine(float efficiency = 0)
     {
-        Debug.Log($"MINE MineralVein {gameObject.name} {efficiency}");
+        Debug.Log($"MineralVein Mine {efficiency} {hardness} {integrity}");
+        lastMine = Time.time;
+        if (efficiency + 1 < hardness) return;
+        integrity -= efficiency / hardness;
+
+        if (integrity <= 0)
+        {
+            integrity = 0;
+            Break();
+        }
+    }
+    private void Break()
+    {
+        Debug.Log($"MineralVein Break");
     }
 
 }
