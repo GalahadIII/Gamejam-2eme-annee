9d3e9105d082110d101777c84c7ea8ac665abe4f
GalahadIII <rphl.levesque@gmail.com>
2023-11-26 15:26:46 -0500

    not working just want to pull

--- miner-game/Assets/Scripts/Player/PlayerPickup.cs
+++ miner-game/Assets/Scripts/Player/PlayerPickup.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 [RequireComponent(typeof(Collider2D))]
 public class PlayerPickup : ObjectColliderDetector2D
 {
+    private Inventory _inventory;
     private Collider2D col;
     // Start is called before the first frame update
     void Start()
     {
+        _inventory = GetComponentInParent<Inventory>();
         col = GetComponent<Collider2D>();
     }
 
@@ -19,10 +25,27 @@ public class PlayerPickup : ObjectColliderDetector2D
         if (Objects.FirstOrDefault())
         {
             GameObject o = Objects.FirstOrDefault();
-
             if (o.CompareTag("Chunk"))
             {
+                // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                IChunk chunk = o.GetComponent<IChunk>();
                 
+                switch(chunk.GetType().ToString())
+                {
+                    case "CoalChunk":
+                        _inventory.AddItem();
+                        break;
+                    case "IronChunk":
+                        _inventory.AddItem();
+                        break;
+                    case "GoldChunk":
+                        _inventory.AddItem();
+                        break;
+                    case "DiamondChunk":
+                        _inventory.AddItem();
+                        break;
+                }
+                Destroy(o);
             }
         }
     }
